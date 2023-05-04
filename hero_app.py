edbfcae0dc14f8e9e55e828ec57dfbd074bd3429
xiaowenshuang <xiaows1989@163.com>
2023-05-04 15:32:22 +0800

    提交作业

--- hero_app.py
+++ hero_app.py
@@ -13,6 +13,10 @@ __desc__ = 'python练习作业'
 
 
 def print_menu():
+    """
+    打印主菜单
+    :return:
+    """
     print("""
         1. **创建英雄**
         2. **查看英雄信息**
@@ -23,12 +27,22 @@ def print_menu():
 
 
 def print_all_heroes(hero_list):
+    """
+    打印英雄列表
+    :param hero_list:
+    :return:
+    """
     print("当前英雄列表如下：")
     for hero in hero_list:
         print(f"{hero_list.index(hero) + 1}. {hero.get('name')}")
 
 
 def add_hero(hero_list):
+    """
+    添加新英雄
+    :param hero_list:
+    :return:
+    """
     hero_name = input("请输入英雄的名称：")
     hero_volume = input("请输入英雄的血量：")
     hero_power = input("请输入英雄的攻击力：")
@@ -38,9 +52,15 @@ def add_hero(hero_list):
 
 
 def view_hero(hero_list):
+    """
+    查看单个英雄详细信息
+    :param hero_list:
+    :return:
+    """
     if hero_list:
         print_all_heroes(hero_list)
         res = int(input("请输入需要查看的英雄编号：")) - 1
+        # TODO：必要的输入校验，例如非整数字符或者越界
         print(f"英雄名称为{hero_list[res]['name']} ，英雄的血量为{hero_list[res]['volume']} ，英雄的攻击力为{hero_list[res]['power']}")
     else:
         print("当前没有任何英雄，请先添加！")
@@ -50,6 +70,7 @@ def update_hero(hero_list):
     if hero_list:
         print_all_heroes(hero_list)
         res = int(input("请输入需要修改的英雄编号：")) - 1
+        # TODO：必要的输入校验，例如非整数字符或者越界
         print(
             f"当前英雄名称为{hero_list[res]['name']} ，英雄的血量为{hero_list[res]['volume']} ，英雄的攻击力为{hero_list[res]['power']}")
         hero_name = input("请输入英雄的新名称：")
@@ -66,6 +87,7 @@ def delete_hero(hero_list):
     if hero_list:
         print_all_heroes(hero_list)
         res = int(input("请输入需要删除的英雄编号：")) - 1
+        # TODO：必要的输入校验，例如非整数字符或者越界
         hero_list.pop(res)
         print("删除成功！")
     else:
